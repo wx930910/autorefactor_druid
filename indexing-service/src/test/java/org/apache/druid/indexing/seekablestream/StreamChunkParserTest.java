90ae9440e11868bbc3b567e5232c9ed6ada4cba1
Wang Xiao <wx19930910@gmail.com>
2021-01-28 17:49:53 -0500

    remove redundant test cases in StreamChunkParserTest.java

--- indexing-service/src/test/java/org/apache/druid/indexing/seekablestream/StreamChunkParserTest.java
+++ indexing-service/src/test/java/org/apache/druid/indexing/seekablestream/StreamChunkParserTest.java
@@ -19,17 +19,11 @@
 
 package org.apache.druid.indexing.seekablestream;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
-import javax.annotation.Nullable;
-
-import org.apache.druid.data.input.InputEntity;
-import org.apache.druid.data.input.InputEntityReader;
 import org.apache.druid.data.input.InputRow;
 import org.apache.druid.data.input.InputRowSchema;
 import org.apache.druid.data.input.impl.DimensionsSpec;
@@ -45,103 +39,45 @@ import org.apache.druid.segment.transform.TransformSpec;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
+import org.mockito.Mockito;
 
 import com.google.common.collect.Iterables;
 
 public class StreamChunkParserTest {
-	private static final TimestampSpec TIMESTAMP_SPEC = new TimestampSpec(null,
-			null, null);
+	private static final TimestampSpec TIMESTAMP_SPEC = new TimestampSpec(null, null, null);
 
 	@Rule
 	public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
-	@Rule
-	public ExpectedException expectedException = ExpectedException.none();
-
 	@Test
-	public void testWithParserAndNullInputformatParseProperly()
-			throws IOException {
-		final InputRowParser<ByteBuffer> parser = new StringInputRowParser(
-				new JSONParseSpec(TIMESTAMP_SPEC, DimensionsSpec.EMPTY,
-						JSONPathSpec.DEFAULT, Collections.emptyMap(), false),
-				StringUtils.UTF8_STRING);
-		final StreamChunkParser chunkParser = new StreamChunkParser(parser,
-				// Set nulls for all parameters below since inputFormat will be
-				// never used.
-				null, null, null, null);
+	public void testBothParserAndInputFormatParseProperlyUsingInputFormat() throws IOException {
+		final InputRowParser<ByteBuffer> parser = new StringInputRowParser(new JSONParseSpec(TIMESTAMP_SPEC,
+				DimensionsSpec.EMPTY, JSONPathSpec.DEFAULT, Collections.emptyMap(), false), StringUtils.UTF8_STRING);
+		final JsonInputFormat inputFormat = Mockito
+				.spy(new JsonInputFormat(JSONPathSpec.DEFAULT, Collections.emptyMap(), null));
+		try {
+			Mockito.doAnswer((stubInvo) -> {
+				return stubInvo.callRealMethod();
+			}).when(inputFormat).createReader(Mockito.any(), Mockito.any(), Mockito.any());
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+		final StreamChunkParser chunkParser = new StreamChunkParser(parser, inputFormat,
+				new InputRowSchema(TIMESTAMP_SPEC, DimensionsSpec.EMPTY, Collections.emptyList()), TransformSpec.NONE,
+				temporaryFolder.newFolder());
 		parseAndAssertResult(chunkParser);
+		Mockito.verify(inputFormat, Mockito.atLeastOnce()).createReader(Mockito.any(), Mockito.any(), Mockito.any());
 	}
 
-	@Test
-	public void testWithNullParserAndInputformatParseProperly()
-			throws IOException {
-		final JsonInputFormat inputFormat = new JsonInputFormat(
-				JSONPathSpec.DEFAULT, Collections.emptyMap(), null);
-		final StreamChunkParser chunkParser = new StreamChunkParser(null,
-				inputFormat,
-				new InputRowSchema(TIMESTAMP_SPEC, DimensionsSpec.EMPTY,
-						Collections.emptyList()),
-				TransformSpec.NONE, temporaryFolder.newFolder());
-		parseAndAssertResult(chunkParser);
-	}
-
-	@Test
-	public void testWithNullParserAndNullInputformatFailToCreateParser() {
-		expectedException.expect(IllegalArgumentException.class);
-		expectedException
-				.expectMessage("Either parser or inputFormat should be set");
-		final StreamChunkParser chunkParser = new StreamChunkParser(null, null,
-				null, null, null);
-	}
-
-	@Test
-	public void testBothParserAndInputFormatParseProperlyUsingInputFormat()
-			throws IOException {
-		final InputRowParser<ByteBuffer> parser = new StringInputRowParser(
-				new JSONParseSpec(TIMESTAMP_SPEC, DimensionsSpec.EMPTY,
-						JSONPathSpec.DEFAULT, Collections.emptyMap(), false),
-				StringUtils.UTF8_STRING);
-		final TrackingJsonInputFormat inputFormat = new TrackingJsonInputFormat(
-				JSONPathSpec.DEFAULT, Collections.emptyMap());
-		final StreamChunkParser chunkParser = new StreamChunkParser(parser,
-				inputFormat,
-				new InputRowSchema(TIMESTAMP_SPEC, DimensionsSpec.EMPTY,
-						Collections.emptyList()),
-				TransformSpec.NONE, temporaryFolder.newFolder());
-		parseAndAssertResult(chunkParser);
-		Assert.assertTrue(inputFormat.used);
-	}
-
-	private void parseAndAssertResult(StreamChunkParser chunkParser)
-			throws IOException {
+	private void parseAndAssertResult(StreamChunkParser chunkParser) throws IOException {
 		final String json = "{\"timestamp\": \"2020-01-01\", \"dim\": \"val\", \"met\": \"val2\"}";
-		List<InputRow> parsedRows = chunkParser.parse(Collections
-				.singletonList(json.getBytes(StringUtils.UTF8_STRING)));
+		List<InputRow> parsedRows = chunkParser
+				.parse(Collections.singletonList(json.getBytes(StringUtils.UTF8_STRING)));
 		Assert.assertEquals(1, parsedRows.size());
 		InputRow row = parsedRows.get(0);
 		Assert.assertEquals(DateTimes.of("2020-01-01"), row.getTimestamp());
-		Assert.assertEquals("val",
-				Iterables.getOnlyElement(row.getDimension("dim")));
-		Assert.assertEquals("val2",
-				Iterables.getOnlyElement(row.getDimension("met")));
-	}
-
-	private static class TrackingJsonInputFormat extends JsonInputFormat {
-		private boolean used;
-
-		private TrackingJsonInputFormat(@Nullable JSONPathSpec flattenSpec,
-				@Nullable Map<String, Boolean> featureSpec) {
-			super(flattenSpec, featureSpec, null);
-		}
-
-		@Override
-		public InputEntityReader createReader(InputRowSchema inputRowSchema,
-				InputEntity source, File temporaryDirectory) {
-			used = true;
-			return super.createReader(inputRowSchema, source,
-					temporaryDirectory);
-		}
+		Assert.assertEquals("val", Iterables.getOnlyElement(row.getDimension("dim")));
+		Assert.assertEquals("val2", Iterables.getOnlyElement(row.getDimension("met")));
 	}
 }
