38fcbc16fa1a9e4ff9e561931bf1fa368621a94f
Wang Xiao <wx19930910@gmail.com>
2021-01-28 18:54:04 -0500

    modify test cases for human study

--- core/src/test/java/org/apache/druid/data/input/HandlingInputRowIteratorTest.java
+++ core/src/test/java/org/apache/druid/data/input/HandlingInputRowIteratorTest.java
@@ -19,6 +19,12 @@
 
 package org.apache.druid.data.input;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.annotation.Nullable;
+
 import org.apache.druid.java.util.common.CloseableIterators;
 import org.apache.druid.java.util.common.parsers.CloseableIterator;
 import org.easymock.EasyMock;
@@ -28,152 +34,63 @@ import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 
-import javax.annotation.Nullable;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.NoSuchElementException;
-
 @RunWith(Enclosed.class)
-public class HandlingInputRowIteratorTest
-{
-  public static class AbsentRowTest
-  {
-    private static final CloseableIterator<InputRow> EMPTY_ITERATOR = CloseableIterators.withEmptyBaggage(
-        new Iterator<InputRow>()
-        {
-          @Override
-          public boolean hasNext()
-          {
-            return false;
-          }
+public class HandlingInputRowIteratorTest {
 
-          @Nullable
-          @Override
-          public InputRow next()
-          {
-            throw new NoSuchElementException();
-          }
-        }
-    );
+	public static class PresentRowTest {
+		private static final InputRow INPUT_ROW1 = EasyMock.mock(InputRow.class);
+		private static final InputRow INPUT_ROW2 = EasyMock.mock(InputRow.class);
+		private static final List<InputRow> INPUT_ROWS = Arrays.asList(INPUT_ROW1, INPUT_ROW2);
 
-    private HandlingInputRowIterator target;
+		private TestInputRowHandler unsuccessfulHandler;
 
-    @Before
-    public void setup()
-    {
-      target = new HandlingInputRowIterator(EMPTY_ITERATOR, Collections.emptyList());
-    }
+		@Before
+		public void setup() {
+			unsuccessfulHandler = new TestInputRowHandler(false);
+		}
 
-    @Test
-    public void doesNotHaveNext()
-    {
-      Assert.assertFalse(target.hasNext());
-    }
+		@Test
+		public void hasNext() {
+			HandlingInputRowIterator target = createInputRowIterator(unsuccessfulHandler, unsuccessfulHandler);
+			Assert.assertTrue(target.hasNext());
+			Assert.assertFalse(unsuccessfulHandler.invoked);
+		}
 
-    @Test(expected = NoSuchElementException.class)
-    public void throwsExceptionWhenYieldingNext()
-    {
-      target.next();
-    }
-  }
+		private static HandlingInputRowIterator createInputRowIterator(
+				HandlingInputRowIterator.InputRowHandler firstHandler,
+				HandlingInputRowIterator.InputRowHandler secondHandler) {
+			CloseableIterator<InputRow> iterator = CloseableIterators.withEmptyBaggage(new Iterator<InputRow>() {
+				private final Iterator<InputRow> delegate = INPUT_ROWS.iterator();
 
-  public static class PresentRowTest
-  {
-    private static final InputRow INPUT_ROW1 = EasyMock.mock(InputRow.class);
-    private static final InputRow INPUT_ROW2 = EasyMock.mock(InputRow.class);
-    private static final List<InputRow> INPUT_ROWS = Arrays.asList(INPUT_ROW1, INPUT_ROW2);
+				@Override
+				public boolean hasNext() {
+					return delegate.hasNext();
+				}
 
-    private TestInputRowHandler successfulHandler;
-    private TestInputRowHandler unsuccessfulHandler;
+				@Nullable
+				@Override
+				public InputRow next() {
+					return delegate.next();
+				}
+			});
 
-    @Before
-    public void setup()
-    {
-      successfulHandler = new TestInputRowHandler(true);
-      unsuccessfulHandler = new TestInputRowHandler(false);
-    }
+			return new HandlingInputRowIterator(iterator, Arrays.asList(firstHandler, secondHandler));
+		}
 
-    @Test
-    public void hasNext()
-    {
-      HandlingInputRowIterator target = createInputRowIterator(unsuccessfulHandler, unsuccessfulHandler);
-      Assert.assertTrue(target.hasNext());
-      Assert.assertFalse(unsuccessfulHandler.invoked);
-    }
+		private static class TestInputRowHandler implements HandlingInputRowIterator.InputRowHandler {
+			boolean invoked = false;
 
-    @Test
-    public void yieldsNextIfUnhandled()
-    {
-      HandlingInputRowIterator target = createInputRowIterator(unsuccessfulHandler, unsuccessfulHandler);
-      Assert.assertEquals(INPUT_ROW1, target.next());
-      Assert.assertTrue(unsuccessfulHandler.invoked);
-    }
+			private final boolean successful;
 
-    @Test
-    public void yieldsNullIfHandledByFirst()
-    {
-      HandlingInputRowIterator target = createInputRowIterator(successfulHandler, unsuccessfulHandler);
-      Assert.assertNull(target.next());
-      Assert.assertTrue(successfulHandler.invoked);
-      Assert.assertFalse(unsuccessfulHandler.invoked);
-    }
+			TestInputRowHandler(boolean successful) {
+				this.successful = successful;
+			}
 
-    @Test
-    public void yieldsNullIfHandledBySecond()
-    {
-      HandlingInputRowIterator target = createInputRowIterator(unsuccessfulHandler, successfulHandler);
-      Assert.assertNull(target.next());
-      Assert.assertTrue(unsuccessfulHandler.invoked);
-      Assert.assertTrue(successfulHandler.invoked);
-    }
-
-    private static HandlingInputRowIterator createInputRowIterator(
-        HandlingInputRowIterator.InputRowHandler firstHandler,
-        HandlingInputRowIterator.InputRowHandler secondHandler
-    )
-    {
-      CloseableIterator<InputRow> iterator = CloseableIterators.withEmptyBaggage(
-          new Iterator<InputRow>()
-          {
-            private final Iterator<InputRow> delegate = INPUT_ROWS.iterator();
-
-            @Override
-            public boolean hasNext()
-            {
-              return delegate.hasNext();
-            }
-
-            @Nullable
-            @Override
-            public InputRow next()
-            {
-              return delegate.next();
-            }
-          }
-      );
-
-      return new HandlingInputRowIterator(iterator, Arrays.asList(firstHandler, secondHandler));
-    }
-
-    private static class TestInputRowHandler implements HandlingInputRowIterator.InputRowHandler
-    {
-      boolean invoked = false;
-
-      private final boolean successful;
-
-      TestInputRowHandler(boolean successful)
-      {
-        this.successful = successful;
-      }
-
-      @Override
-      public boolean handle(InputRow inputRow)
-      {
-        invoked = true;
-        return successful;
-      }
-    }
-  }
+			@Override
+			public boolean handle(InputRow inputRow) {
+				invoked = true;
+				return successful;
+			}
+		}
+	}
 }
--- indexing-service/src/test/java/org/apache/druid/indexing/seekablestream/StreamChunkParserTest.java
+++ indexing-service/src/test/java/org/apache/druid/indexing/seekablestream/StreamChunkParserTest.java
@@ -45,87 +45,38 @@ import org.apache.druid.segment.transform.TransformSpec;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
 
 import com.google.common.collect.Iterables;
 
 public class StreamChunkParserTest {
-	private static final TimestampSpec TIMESTAMP_SPEC = new TimestampSpec(null,
-			null, null);
+	private static final TimestampSpec TIMESTAMP_SPEC = new TimestampSpec(null, null, null);
 
 	@Rule
 	public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
-	@Rule
-	public ExpectedException expectedException = ExpectedException.none();
-
 	@Test
-	public void testWithParserAndNullInputformatParseProperly()
-			throws IOException {
-		final InputRowParser<ByteBuffer> parser = new StringInputRowParser(
-				new JSONParseSpec(TIMESTAMP_SPEC, DimensionsSpec.EMPTY,
-						JSONPathSpec.DEFAULT, Collections.emptyMap(), false),
-				StringUtils.UTF8_STRING);
-		final StreamChunkParser chunkParser = new StreamChunkParser(parser,
-				// Set nulls for all parameters below since inputFormat will be
-				// never used.
-				null, null, null, null);
-		parseAndAssertResult(chunkParser);
-	}
-
-	@Test
-	public void testWithNullParserAndInputformatParseProperly()
-			throws IOException {
-		final JsonInputFormat inputFormat = new JsonInputFormat(
-				JSONPathSpec.DEFAULT, Collections.emptyMap(), null);
-		final StreamChunkParser chunkParser = new StreamChunkParser(null,
-				inputFormat,
-				new InputRowSchema(TIMESTAMP_SPEC, DimensionsSpec.EMPTY,
-						Collections.emptyList()),
-				TransformSpec.NONE, temporaryFolder.newFolder());
-		parseAndAssertResult(chunkParser);
-	}
-
-	@Test
-	public void testWithNullParserAndNullInputformatFailToCreateParser() {
-		expectedException.expect(IllegalArgumentException.class);
-		expectedException
-				.expectMessage("Either parser or inputFormat should be set");
-		final StreamChunkParser chunkParser = new StreamChunkParser(null, null,
-				null, null, null);
-	}
-
-	@Test
-	public void testBothParserAndInputFormatParseProperlyUsingInputFormat()
-			throws IOException {
-		final InputRowParser<ByteBuffer> parser = new StringInputRowParser(
-				new JSONParseSpec(TIMESTAMP_SPEC, DimensionsSpec.EMPTY,
-						JSONPathSpec.DEFAULT, Collections.emptyMap(), false),
-				StringUtils.UTF8_STRING);
-		final TrackingJsonInputFormat inputFormat = new TrackingJsonInputFormat(
-				JSONPathSpec.DEFAULT, Collections.emptyMap());
-		final StreamChunkParser chunkParser = new StreamChunkParser(parser,
-				inputFormat,
-				new InputRowSchema(TIMESTAMP_SPEC, DimensionsSpec.EMPTY,
-						Collections.emptyList()),
-				TransformSpec.NONE, temporaryFolder.newFolder());
+	public void testBothParserAndInputFormatParseProperlyUsingInputFormat() throws IOException {
+		final InputRowParser<ByteBuffer> parser = new StringInputRowParser(new JSONParseSpec(TIMESTAMP_SPEC,
+				DimensionsSpec.EMPTY, JSONPathSpec.DEFAULT, Collections.emptyMap(), false), StringUtils.UTF8_STRING);
+		final TrackingJsonInputFormat inputFormat = new TrackingJsonInputFormat(JSONPathSpec.DEFAULT,
+				Collections.emptyMap());
+		final StreamChunkParser chunkParser = new StreamChunkParser(parser, inputFormat,
+				new InputRowSchema(TIMESTAMP_SPEC, DimensionsSpec.EMPTY, Collections.emptyList()), TransformSpec.NONE,
+				temporaryFolder.newFolder());
 		parseAndAssertResult(chunkParser);
 		Assert.assertTrue(inputFormat.used);
 	}
 
-	private void parseAndAssertResult(StreamChunkParser chunkParser)
-			throws IOException {
+	private void parseAndAssertResult(StreamChunkParser chunkParser) throws IOException {
 		final String json = "{\"timestamp\": \"2020-01-01\", \"dim\": \"val\", \"met\": \"val2\"}";
-		List<InputRow> parsedRows = chunkParser.parse(Collections
-				.singletonList(json.getBytes(StringUtils.UTF8_STRING)));
+		List<InputRow> parsedRows = chunkParser
+				.parse(Collections.singletonList(json.getBytes(StringUtils.UTF8_STRING)));
 		Assert.assertEquals(1, parsedRows.size());
 		InputRow row = parsedRows.get(0);
 		Assert.assertEquals(DateTimes.of("2020-01-01"), row.getTimestamp());
-		Assert.assertEquals("val",
-				Iterables.getOnlyElement(row.getDimension("dim")));
-		Assert.assertEquals("val2",
-				Iterables.getOnlyElement(row.getDimension("met")));
+		Assert.assertEquals("val", Iterables.getOnlyElement(row.getDimension("dim")));
+		Assert.assertEquals("val2", Iterables.getOnlyElement(row.getDimension("met")));
 	}
 
 	private static class TrackingJsonInputFormat extends JsonInputFormat {
@@ -137,11 +88,10 @@ public class StreamChunkParserTest {
 		}
 
 		@Override
-		public InputEntityReader createReader(InputRowSchema inputRowSchema,
-				InputEntity source, File temporaryDirectory) {
+		public InputEntityReader createReader(InputRowSchema inputRowSchema, InputEntity source,
+				File temporaryDirectory) {
 			used = true;
-			return super.createReader(inputRowSchema, source,
-					temporaryDirectory);
+			return super.createReader(inputRowSchema, source, temporaryDirectory);
 		}
 	}
 }
