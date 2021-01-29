bfa0bdc4a3d88f03959ea37687d25d5aebd7ed77
Wang Xiao <wx19930910@gmail.com>
2021-01-28 19:14:26 -0500

    update CpuAcctDeltaMonitorTest.java for human-study

--- core/src/test/java/org/apache/druid/java/util/metrics/CpuAcctDeltaMonitorTest.java
+++ core/src/test/java/org/apache/druid/java/util/metrics/CpuAcctDeltaMonitorTest.java
@@ -19,83 +19,45 @@
 
 package org.apache.druid.java.util.metrics;
 
-import com.google.common.collect.ImmutableMap;
-import org.apache.druid.java.util.common.StringUtils;
+import java.io.File;
+import java.io.IOException;
+
 import org.apache.druid.java.util.metrics.cgroups.TestUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import com.google.common.collect.ImmutableMap;
 
-public class CpuAcctDeltaMonitorTest
-{
-  @Rule
-  public ExpectedException expectedException = ExpectedException.none();
-  @Rule
-  public TemporaryFolder temporaryFolder = new TemporaryFolder();
-  private File procDir;
-  private File cgroupDir;
-  private File cpuacctDir;
+public class CpuAcctDeltaMonitorTest {
+	@Rule
+	public TemporaryFolder temporaryFolder = new TemporaryFolder();
+	private File procDir;
+	private File cgroupDir;
+	private File cpuacctDir;
 
-  @Before
-  public void setUp() throws IOException
-  {
-    cgroupDir = temporaryFolder.newFolder();
-    procDir = temporaryFolder.newFolder();
-    TestUtils.setUpCgroups(procDir, cgroupDir);
-    cpuacctDir = new File(
-        cgroupDir,
-        "cpu,cpuacct/system.slice/some.service/f12ba7e0-fa16-462e-bb9d-652ccc27f0ee"
-    );
-    Assert.assertTrue((cpuacctDir.isDirectory() && cpuacctDir.exists()) || cpuacctDir.mkdirs());
-    TestUtils.copyResource("/cpuacct.usage_all", new File(cpuacctDir, "cpuacct.usage_all"));
-  }
+	@Before
+	public void setUp() throws IOException {
+		cgroupDir = temporaryFolder.newFolder();
+		procDir = temporaryFolder.newFolder();
+		TestUtils.setUpCgroups(procDir, cgroupDir);
+		cpuacctDir = new File(cgroupDir, "cpu,cpuacct/system.slice/some.service/f12ba7e0-fa16-462e-bb9d-652ccc27f0ee");
+		Assert.assertTrue((cpuacctDir.isDirectory() && cpuacctDir.exists()) || cpuacctDir.mkdirs());
+		TestUtils.copyResource("/cpuacct.usage_all", new File(cpuacctDir, "cpuacct.usage_all"));
+	}
 
-  @Test
-  public void testMonitorWontCrash()
-  {
-    final CpuAcctDeltaMonitor monitor = new CpuAcctDeltaMonitor(
-        "some_feed",
-        ImmutableMap.of(),
-        cgroup -> {
-          throw new RuntimeException("Should continue");
-        }
-    );
-    final StubServiceEmitter emitter = new StubServiceEmitter("service", "host");
-    monitor.doMonitor(emitter);
-    monitor.doMonitor(emitter);
-    monitor.doMonitor(emitter);
-    Assert.assertTrue(emitter.getEvents().isEmpty());
-  }
+	@Test
+	public void testMonitorWontCrash() {
+		final CpuAcctDeltaMonitor monitor = new CpuAcctDeltaMonitor("some_feed", ImmutableMap.of(), cgroup -> {
+			throw new RuntimeException("Should continue");
+		});
+		final StubServiceEmitter emitter = new StubServiceEmitter("service", "host");
+		monitor.doMonitor(emitter);
+		monitor.doMonitor(emitter);
+		monitor.doMonitor(emitter);
+		Assert.assertTrue(emitter.getEvents().isEmpty());
+	}
 
-  @Test
-  public void testSimpleMonitor() throws Exception
-  {
-    final File cpuacct = new File(cpuacctDir, "cpuacct.usage_all");
-    try (final FileOutputStream fos = new FileOutputStream(cpuacct)) {
-      fos.write(StringUtils.toUtf8("cpu user system\n"));
-      for (int i = 0; i < 128; ++i) {
-        fos.write(StringUtils.toUtf8(StringUtils.format("%d 0 0\n", i)));
-      }
-    }
-    final CpuAcctDeltaMonitor monitor = new CpuAcctDeltaMonitor(
-        "some_feed",
-        ImmutableMap.of(),
-        (cgroup) -> cpuacctDir.toPath()
-    );
-    final StubServiceEmitter emitter = new StubServiceEmitter("service", "host");
-    Assert.assertFalse(monitor.doMonitor(emitter));
-    // First should just cache
-    Assert.assertEquals(0, emitter.getEvents().size());
-    Assert.assertTrue(cpuacct.delete());
-    TestUtils.copyResource("/cpuacct.usage_all", cpuacct);
-    Assert.assertTrue(monitor.doMonitor(emitter));
-    Assert.assertEquals(2 * 128 + 1, emitter.getEvents().size());
-  }
 }
